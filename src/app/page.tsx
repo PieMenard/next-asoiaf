f4cf5696c0c124f5657a91728aa133478cae5304
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-10-04 09:32:54 -0400

    add pagination and loading message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 'use client';
 
 import { Character } from '@/types/types';
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 
 export default function Home() {
   const [characters, setCharacters] = useState<Character[]>([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     const fetchCharacterList = async () => {
       try {
         const res = await fetch(
-          `https://anapioficeandfire.com/api/characters?page=10`
+          `https://anapioficeandfire.com/api/characters?page=${page}`
         );
         const data = await res.json();
 
@@ -29,31 +32,53 @@ export default function Home() {
           };
         });
         const charactersInfo = await Promise.all(fetchDetails);
-        console.log(charactersInfo);
         setCharacters(charactersInfo);
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchCharacterList();
-  }, []);
+  }, [page]);
 
   return (
-    <main>
-      <ul className="my-5 mx-5">
-        {characters.map((char) => (
-          <li key={char.id} className="my-5">
-            <h1 className="font-semibold">
-              {char.id}. {char.name} - {char.gender}
-            </h1>
-            <ul>
-              {char.houses.map((house, index) => (
-                <li key={index}>{house}</li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+    <main className="text-center">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="my-5 mx-5">
+          {characters.map((char) => (
+            <li key={char.id} className="my-5">
+              <h1 className="font-semibold">
+                {char.id}. {char.name} - {char.gender}
+              </h1>
+              <ul>
+                {char.houses.map((house, index) => (
+                  <li key={index}>{house}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      <div>
+        <button
+          onClick={() => setPage(page - 1)}
+          disabled={page <= 1}
+          className="rounded-md bg-slate-300 px-2 mx-2 hover:bg-slate-400 disabled:opacity-40 disabled:pointer-events-none"
+        >
+          Prev
+        </button>
+        {page}
+        <button
+          onClick={() => setPage(page + 1)}
+          className="rounded-md bg-slate-300 px-2 mx-2 hover:bg-slate-400"
+        >
+          Next
+        </button>
+      </div>
     </main>
   );
 }
