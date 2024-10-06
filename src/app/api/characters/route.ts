ec1878d55cdf18617015821c749e31c816f52ea8
PieMenard <pinedo.m.gustavo@gmail.com>
2024-10-06 01:30:50 -0400

    add route to get all characters with pagination

--- src/app/api/characters/route.ts
+++ src/app/api/characters/route.ts
@@ -26,3 +26,29 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ success: false, error: error }, { status: 400 });
   }
 }
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const offset = parseInt(searchParams.get('offset') || '0');
+  const limit = parseInt(searchParams.get('limit') || '10');
+  try {
+    const characters = await prisma.character.findMany({
+      select: {
+        name: true,
+        id: true,
+      },
+      skip: offset,
+      take: limit,
+    });
+
+    const results = {
+      offset: offset,
+      limit: limit,
+      results: characters,
+    };
+
+    return NextResponse.json({ success: true, data: results });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error }, { status: 400 });
+  }
+}
