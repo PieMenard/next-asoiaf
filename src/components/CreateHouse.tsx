6fe57f87efb36b99e30d892f2027499b2adb64d6
PieMenard <pinedo.m.gustavo@gmail.com>
2024-10-06 21:48:09 -0400

    add create house modal

--- src/components/CreateHouse.tsx
+++ src/components/CreateHouse.tsx
@@ -1,8 +1,9 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -10,13 +11,46 @@ import {
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Textarea } from './ui/textarea';
+import { FormEvent, useRef, useState } from 'react';
 
 export function CreateHouse() {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    words: '',
+  });
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('api/houses', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success) {
+        formRef.current?.reset();
+        setFormData({
+          name: '',
+          words: '',
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setDialogOpen(false);
+  };
   return (
     <div className="my-3">
-      <Dialog>
+      <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
         <DialogTrigger asChild>
-          <Button variant="secondary" className="bg-slate-300">
+          <Button
+            variant="secondary"
+            className="bg-slate-300"
+            onClick={() => setDialogOpen(true)}
+          >
             Add a House
           </Button>
         </DialogTrigger>
@@ -24,31 +58,41 @@ export function CreateHouse() {
           <DialogHeader>
             <DialogTitle>Create a new House</DialogTitle>
           </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Name
-              </Label>
-              <Input
-                id="name"
-                //   defaultValue="Pedro Duarte"
-                className="col-span-3"
-              />
+          <form onSubmit={handleSubmit}>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input
+                  name="name"
+                  onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                  }
+                  //   defaultValue="Pedro Duarte"
+                  value={formData.name}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Motto
+                </Label>
+                <Textarea
+                  name="username"
+                  onChange={(e) =>
+                    setFormData({ ...formData, words: e.target.value })
+                  }
+                  //   defaultValue="@peduarte"
+                  value={formData.words}
+                  className="col-span-3"
+                />
+              </div>
             </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="username" className="text-right">
-                Motto
-              </Label>
-              <Input
-                id="username"
-                //   defaultValue="@peduarte"
-                className="col-span-3"
-              />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Submit</Button>
-          </DialogFooter>
+            <DialogFooter>
+              <Button type="submit">Submit</Button>
+            </DialogFooter>
+          </form>
         </DialogContent>
       </Dialog>
     </div>
